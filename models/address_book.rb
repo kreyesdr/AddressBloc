008bccd02460ce5874ef6b19526699626f113721
kreyesdr <kreyes616@gmail.com>
2016-05-18 17:00:11 -0400

    Adding remove_entry method/spec

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,7 +1,7 @@
 require_relative 'entry'
 
-class AdressBook
-    attr_reader :entries
+class AddressBook
+  attr_reader :entries
 
   def initialize
     @entries = []
@@ -13,9 +13,22 @@ class AdressBook
       if name < entry.name
         break
       end
-      index += 1
+      index += index
     end
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+    end
+    @entries.delete(delete_entry)
+  end
 end
+
+
+
 
